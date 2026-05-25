922af3ddff8b257d0390597b81b69ab41903277c
danushkadi <danushkad@ccl.lk>
2026-05-25 11:40:30 +0530

    feat:added authguard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { UserListComponent } from './features/dashboard/user-list/user-list.component';
 import { UserDetailComponent } from './features/dashboard/user-detail/user-detail.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -11,9 +12,11 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: UserListComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'dashboard/user-details/:id',
     component: UserDetailComponent,
+    canActivate: [AuthGuard],
   },
 ];
