ba72479b7f38b4e4bde0010f489db0cd68f7c799
danushkadi <danushkad@ccl.lk>
2026-05-25 11:06:54 +0530

    feat:added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { UserListComponent } from './features/dashboard/user-list/user-list.component';
+import { UserDetailComponent } from './features/dashboard/user-detail/user-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'user-list',
+    component: UserListComponent,
+  },
+  {
+    path: 'user-list/:id',
+    component: UserDetailComponent,
+  },
+];
